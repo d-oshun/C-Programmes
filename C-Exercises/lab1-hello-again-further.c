74b0422ded84db2fc64ed854480c8410abf015ae
Diana Williams Oshun <145607859+d-oshun@users.noreply.github.com>
2023-09-23 11:08:57 +0100

    Add files via upload

---
+++ C-Exercises/lab1-hello-again-further.c
@@ -0,0 +1,20 @@
+/*
+Program: lab1-hello-again-further.c
+Author: Diana Williams Oshun
+Student Number: 23101961
+Input: A full name
+Output: Print out the given name
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[])
+{
+    char your_name;
+
+    printf("Hello\n ");
+    scanf("%s", your_name);
+
+    return(0);
+}
